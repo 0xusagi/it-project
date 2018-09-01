51abcb462fa4524fcc29dbfbbc88599c42a2c323
aangelatos <37162995+aangelatos@users.noreply.github.com>
2018-09-02 08:57:07 +1000

    Update StartActivity.java

--- SpainITproject/app/src/main/java/com/comp30023/spain_itproject/StartActivity.java
+++ SpainITproject/app/src/main/java/com/comp30023/spain_itproject/StartActivity.java
@@ -1,13 +1,36 @@
 package com.comp30023.spain_itproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        button = (Button) findViewById(R.id.login_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                /*
+                 * Intent is just like glue which helps to navigate one activity
+                 * to another.
+                 */
+                Intent intent = new Intent(StartActivity.this,
+                        LoginActivity.class);
+                startActivity(intent); // startActivity allow you to move
+            }
+        });
     }
 }
