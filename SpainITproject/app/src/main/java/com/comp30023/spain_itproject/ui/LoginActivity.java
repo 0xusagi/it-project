b1c107931e530b4220df18ed40a69999615f849f
Samuel Wickham <s.wickham@outlook.com.au>
2018-09-07 12:26:12 +1000

    Integrated LoginActivity

--- SpainITproject/app/src/main/java/com/comp30023/spain_itproject/ui/LoginActivity.java
+++ SpainITproject/app/src/main/java/com/comp30023/spain_itproject/ui/LoginActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 
 import com.comp30023.spain_itproject.LoginHandler;
@@ -15,15 +17,24 @@ import com.comp30023.spain_itproject.domain.Location;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final int PIN_LENGTH = 4;
+
+    private Button loginButton;
     private Button cancelButton;
 
     private ToggleButton dependentButton;
     private ToggleButton carerButton;
 
+    private EditText phoneNumberText;
+    private EditText pinText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        setLoginButtonListener(this);
 
         cancelButton = (Button) findViewById(R.id.cancelButton);
         setCancelButtonListener(this);
@@ -55,6 +66,20 @@ public class LoginActivity extends AppCompatActivity {
                 carerButton.setChecked(true);
             }
         });
+
+        phoneNumberText = (EditText) findViewById(R.id.phoneNumberLoginField);
+
+        setPinFields();
+    }
+
+    private void setPinFields() {
+
+        pinText = (EditText) findViewById(R.id.pinField);
+
+        InputFilter.LengthFilter maxPinLength = new InputFilter.LengthFilter(PIN_LENGTH);
+        InputFilter[] pinFilters = new InputFilter[1];
+        pinFilters[0] = maxPinLength;
+        pinText.setFilters(pinFilters);
     }
 
     private void setCancelButtonListener(final Context context) {
@@ -67,6 +92,31 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(context, StartActivity.class);
                 startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    /**STILL REQUIRES CHECKING FOR INVALID DETAILS*/
+    private void setLoginButtonListener(final Context context) {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Collects information from other fields and logs in
+             * @param v
+             */
+            @Override
+            public void onClick(View v) {
+
+                //Retrieve inputs from fields
+                String phoneNumber = phoneNumberText.getText().toString();
+                String pin = pinText.getText().toString();
+
+                ToggleButton dependentButton = (ToggleButton) findViewById(R.id.dependentButton);
+                Boolean isDependent = dependentButton.isChecked();
+
+                //Login the user
+                LoginHandler.newLogin(context, phoneNumber, pin, isDependent);
                 finish();
             }
         });
