43406fcc8a6ea0ae69652786524d5690456db567
Oliver Murray <murrayo@student.unimelb.edu.au>
2018-09-11 21:22:26 +1000

    Server changes

--- SpainServer/app.js
+++ SpainServer/app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('./models/db');
-import { Location } from "./models/location";
-import { Dependent } from "./models/user";
-import { User } from "./models/user";
-import { Carer } from "./models/user";
+const index = require('./routes/index');
+import { router } from './routes/index';
+import { createUser } from "./controllers/users"
 
-const app = express();
+createUser('0457 813 001', 'Oliver Murray', 'mypassword');
 
-console.log("Oli: start the mongo shell and run 'use spain-server'");
-console.log("Then run  db.placeholder.insert({name: \"Hello World});");
-console.log("The database should work now");
+export var app = express();
+app.use('/', index);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("App listening on port 3000");
