687e28da76f9187e41d460bed8ef98aa854ac391
Samuel Wickham <s.wickham@outlook.com.au>
2018-09-11 20:11:19 +1000

    Service instantiated

--- SpainITproject/app/src/main/java/com/comp30023/spain_itproject/uicontroller/AccountController.java
+++ SpainITproject/app/src/main/java/com/comp30023/spain_itproject/uicontroller/AccountController.java
@@ -1,6 +1,11 @@
 package com.comp30023.spain_itproject.uicontroller;
 
+import android.accounts.Account;
+import android.widget.Toast;
+
 import com.comp30023.spain_itproject.domain.DependentUser;
+import com.comp30023.spain_itproject.network.AccountService;
+import com.comp30023.spain_itproject.network.RetrofitClientInstance;
 import com.comp30023.spain_itproject.validation.DetailsValidator;
 import com.comp30023.spain_itproject.validation.InvalidDetailsException;
 import com.comp30023.spain_itproject.domain.Location;
@@ -8,9 +13,12 @@ import com.comp30023.spain_itproject.domain.User;
 
 public class AccountController {
 
+    private static AccountService service;
     private static DetailsValidator validator;
 
     public static boolean registerAccount(String name, String phoneNumber, String pin, String confirmPin, Boolean isDependent) throws InvalidDetailsException {
+
+        checkService();
 
         if (validator == null) {
             validator = DetailsValidator.getInstance();
@@ -23,6 +31,8 @@ public class AccountController {
 
     public static User login(String phoneNumber, String pin, boolean isDependent) {
 
+        checkService();
+
         DependentUser user = new DependentUser(null, null, null);
         user.addLocation(new Location(null,"Home"));
         user.addLocation(new Location(null,"Church"));
@@ -31,6 +41,12 @@ public class AccountController {
         user.addLocation(new Location(null,"Shop2"));
 
         return user;
+    }
+
+    public static void checkService() {
+        if (service == null) {
+            service = RetrofitClientInstance.getRetrofitInstance().create(AccountService.class);
+        }
     }
 
 }
