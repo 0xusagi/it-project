897418ab610699c43c1927e044478c500d463a0b
aangelatos <aangelatos@student.unimelb.edu.au>
2018-09-11 19:10:50 +1000

    added serialized tags to address class

--- SpainITproject/app/src/main/java/com/comp30023/spain_itproject/domain/Address.java
+++ SpainITproject/app/src/main/java/com/comp30023/spain_itproject/domain/Address.java
@@ -1,13 +1,25 @@
 package com.comp30023.spain_itproject.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class Address implements Serializable {
 
+
+    @SerializedName("addressLine1")
     private String addressLine1;
+
+    @SerializedName("addressLine2")
     private String addressLine2;
+
+    @SerializedName("postCode")
     private int postCode;
+
+    @SerializedName("city")
     private String city;
+
+    @SerializedName("state")
     private String state;
 
     public Address(String addressLine1, String addressLine2, int postCode, String city, String suburb, String state) {
