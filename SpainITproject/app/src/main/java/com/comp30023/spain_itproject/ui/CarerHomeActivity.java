2e087c1221acd6f0ae7f00a8223c5d69fbbbdffc
wz <wleong3@student.unimelb.edu.au>
2018-09-13 10:25:14 +1000

    Edited the carer home screen to be more effcient

--- SpainITproject/app/src/main/java/com/comp30023/spain_itproject/ui/CarerHomeActivity.java
+++ SpainITproject/app/src/main/java/com/comp30023/spain_itproject/ui/CarerHomeActivity.java
@@ -1,10 +1,16 @@
 package com.comp30023.spain_itproject.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.comp30023.spain_itproject.LoginHandler;
 import com.comp30023.spain_itproject.R;
 import com.comp30023.spain_itproject.domain.DependentUser;
 
@@ -18,9 +24,16 @@ import java.util.ArrayList;
  */
 public class CarerHomeActivity extends AppCompatActivity {
 
+    // Dependents list
     private RecyclerView dependentsListView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    // Settings button
+    private ImageButton settingsButton;
+
+    // Add Dependent button
+    private Button addDependentsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +41,17 @@ public class CarerHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_carer_home);
 
         displayDependentsList();
+
+        setupAddDependentButton();
+
+        setupSettingsButton(this);
     }
 
 
+    /**
+     * Setup displaying the dependents list
+     * Dynamically gets the list of dependents from the server
+     */
     private void displayDependentsList() {
         // TODO Get dependents list from server
         // For now display a fake list
@@ -51,5 +72,36 @@ public class CarerHomeActivity extends AppCompatActivity {
         // Set an adapter
         adapter = new DependentsListAdapter(dependentUsers);
         dependentsListView.setAdapter(adapter);
+    }
+
+    /**
+     * Setup the add dependents button so that it displays the next activity on click
+     */
+    private void setupAddDependentButton() {
+        addDependentsButton = (Button) findViewById(R.id.carerHome_addDependentButton);
+
+        // When pressed the add dependents button the next activity to be shown is the
+        // add dependent activity
+        addDependentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Display the next activity
+                Intent intent = new Intent(getApplicationContext(), AddDependentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setupSettingsButton(final Context context) {
+        settingsButton = (ImageButton) findViewById(R.id.carerHome_settingsButton);
+
+        // When pressed, the user is logged out
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginHandler.logout(context);
+                finish();
+            }
+        });
     }
 }
