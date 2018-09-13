40e617b9811778d55d7ff2f9e2686215ae5ba83c
Oliver Murray <murrayo@student.unimelb.edu.au>
2018-09-13 20:07:02 +1000

    deprecated mongoose.connect parameters

--- SpainServer/models/db.js
+++ SpainServer/models/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import config from '../config/config';
 
-mongoose.connect(config[process.env.NODE_ENV].DBHost, (err) => {
+mongoose.connect(config[process.env.NODE_ENV].DBHost, { useNewUrlParser: true }, (err) => {
     if (!err) {
         console.log("Connected to " + config[process.env.NODE_ENV].DBHost);
     } else {
