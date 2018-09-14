871292153688d089fdf049b533b81d66e0a9fa46
Hector Kemp <hectorakemp@gmail.com>
2018-09-14 16:15:30 +1000

    changed node env

--- SpainServer/app.js
+++ SpainServer/app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// Tells node what the environment is (So we can use a 'clean' test database when using mocha for tests)
-
 import { router } from "./routes";
 
 const db = require('./models/db');
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use('/', router);
 
 app.listen(process.env.PORT || 3000, () => {
