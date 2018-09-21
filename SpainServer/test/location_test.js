121a8db2755bc20c7a1183aa959bfc78f5bab1ca
Oliver Murray <murrayo@student.unimelb.edu.au>
2018-09-21 11:33:04 +1000

    added locations/new test

--- SpainServer/test/location_test.js
+++ SpainServer/test/location_test.js
@@ -13,3 +13,32 @@ let expect = chai.expect;
 chai.use(chaiHttp);
 
 import { Location } from "../models/location";
+
+describe('Locations', () => {
+    /**
+     * Test POST /locations/new
+     */
+    const sampleLocation = {
+        lat: 37.8138,
+        long: 144.9641,
+        displayName: "Brunetti Cafe",
+        addressLine1: "250 Flinders Ln",
+        addressLine2: "Melbourne, VIC",
+        postcode: "3000",
+        state: "VIC",
+        description: "Italian cafe known for sweets, serving pizza and pasta in a modern space with an outdoor area.",
+        imageUrl: "https://cdn.concreteplayground.com/content/uploads/2015/03/Vertue-Coffee-Drink-Carlton-Melbourne-01.jpeg",
+        popularity: 5
+    };
+
+    it('should create a new location', (done) => {
+        chai.request(app)
+            .post('/locations/new')
+            .type('form')
+            .send(sampleLocation)
+            .end((err, res) => {
+                res.should.have.status(201);
+                done();
+            })
+    });
+});
