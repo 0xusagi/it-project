3def5db93381e58a67bcbd41fbe52faa6a38feb1
Samuel Wickham <s.wickham@outlook.com.au>
2018-10-05 21:02:12 +1000

    Gets current location

--- SpainITproject/app/src/main/java/com/comp30023/spain_itproject/DisplayHelpRequestActivity.java
+++ SpainITproject/app/src/main/java/com/comp30023/spain_itproject/DisplayHelpRequestActivity.java
@@ -1,14 +1,24 @@
 package com.comp30023.spain_itproject;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+
+import static com.comp30023.spain_itproject.ui.carerhome.CarerMapsActivity.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
 
 public class DisplayHelpRequestActivity extends AppCompatActivity {
 
@@ -19,11 +29,28 @@ public class DisplayHelpRequestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_help_request);
 
+
+        final Context context = this;
+        final Activity activity = this;
+
         SupportMapFragment fragment = new SupportMapFragment();
         fragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 map = googleMap;
+
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    map.setMyLocationEnabled(true);
+                }
+                else {
+
+                    ActivityCompat.requestPermissions(activity,
+                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                }
+
             }
         });
 
@@ -32,6 +59,7 @@ public class DisplayHelpRequestActivity extends AppCompatActivity {
 
         transaction.replace(R.id.displayHelpRequest_mapFrame, fragment);
         transaction.commit();
+        
 
     }
 }
